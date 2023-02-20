a87c1e773d6a93ac07c22b91b888cef4cce6f1ad
abhijeet <abhigadlinge@gmail.com>
2023-02-20 22:56:58 +0530

    third commit

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ var swiper = new Swiper(".swiper", {
     effect: "coverflow",
     spaceBetween: 30,
     grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+     },
     coverflowEffect: {
        rotate: 50,
        stretch: 0,
@@ -66,21 +69,3 @@ var swiper = new Swiper(".swiper", {
 
 
  
-// var swiper = new Swiper(".reviews-slider", {
-//     loop:true,
-//     slidesPerView: "auto",
-//     grabCursor: true,
-//     spaceBetween: 30,
-//     pagination: {
-//        el: ".swiper-pagination",
-//     },
-//     breakpoints: {
-//        768: {
-//          slidesPerView: 1,
-//        },
-//        991: {
-//          slidesPerView: 2,
-//        },
-//     },
-//  });
-
